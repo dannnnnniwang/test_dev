a9d342182b8e8bd54505b646ca4b42b686248543
dannnnnniwang <334759177@qq.com>
2021-04-13 17:53:55 +0800

    1

--- test_platfrom1/personal/views.py
+++ test_platfrom1/personal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+
+    name = request.GET.get("name","")
+
+    if name == "":
+        return HttpResponse("name is NoneType")
+
+    return HttpResponse("Hello"  +  name)
